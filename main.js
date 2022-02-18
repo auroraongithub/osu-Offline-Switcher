4f1b273b792d8241dbf30950c9b64f284f95b732
Alex <alex.tyshang@gmail.com>
2022-02-18 22:34:41 +1300

    fixed bugs

--- main.js
+++ main.js
@@ -1,60 +1,61 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const sudo = require('sudo-prompt');
 const { dialog } = require('electron')
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+function createWindow() {
+    // Create the browser window.
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+        }
+    })
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+    // and load the index.html of the app.
+    mainWindow.loadFile('index.html')
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  ipcMain.handle("poopie", (event, arg) => {
-    switcher(arg["path"], arg["args"])
-  })
-  ipcMain.handle("anotherpoopie", (event, arg) => {
-    event.reply("sheesh",ipcMain.dialog.showOpenDialog({ properties: ['openFile'], filters:[{ name: "executables", extensions: ['exe']}] }))
-   })
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    createWindow()
+    ipcMain.handle("poopie", (event, arg) => {
+        switcher(arg["path"], arg["args"])
+    })
+    ipcMain.handle("anotherpoopie", (event, arg) => {
+        filePath = dialog.showOpenDialogSync({ properties: ['openFile'], filters: [{ name: "executables", extensions: ['exe'] }] })
+        event.sender.send("sheesh", filePath)
+    })
+    app.on('activate', function() {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', function() {
+    if (process.platform !== 'darwin') app.quit()
 })
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 function switcher(filePath, args) {
-  const options = {
-       name: 'osuoffline'
-  }
-  sudo.exec(`${filePath} ${args}`, options,
-    function(error, stdout, stderr) {
-      if (error) throw error;
-      console.log('stdout: ' + stdout);
-    });
+    const options = {
+        name: 'osuoffline'
+    }
+    sudo.exec(`${filePath} ${args}`, options,
+        function(error, stdout, stderr) {
+            if (error) throw error;
+            console.log('stdout: ' + stdout);
+        });
 }
--- renderer.js
+++ renderer.js
@@ -1,14 +1,14 @@
-window.main.recieve("sheesh" , (arg) => {
-    window.main.send("poopie", {path: "./Firewall Rule Setup.bat", args: arg})
+window.main.receive("sheesh", (arg) => {
+        window.main.send("poopie", { path: "./Firewall Rule Setup.bat", args: arg })
     })
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process because
-// `nodeIntegration` is turned off. Use `preload.js` to
-// selectively enable features needed in the rendering
-// process.
+    // This file is required by the index.html file and will
+    // be executed in the renderer process for that window.
+    // No Node.js APIs are available in this process because
+    // `nodeIntegration` is turned off. Use `preload.js` to
+    // selectively enable features needed in the rendering
+    // process.
 btnswitch.addEventListener('click', () => {
-    window.main.send("poopie", {path: "./osu! offline switcher.bat", args: null})
+    window.main.send("poopie", { path: "./osu! offline switcher.bat", args: null })
 })
 btnrule.addEventListener('click', () => {
     window.main.send("anotherpoopie")
